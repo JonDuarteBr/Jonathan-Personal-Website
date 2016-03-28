0a8d9e1ee05d5173991bb0fb5377cf238bc0e628
JonDuarteBr <jonrodriguesbr@gmail.com>
2016-03-28 17:56:35 -0400

    new folder

---
+++ website.js
@@ -0,0 +1,30 @@
+
+function MoreAndLess1() {
+	if (document.getElementById("dropdown1").innerHTML == "more") {
+	document.getElementById("dropdown1").innerHTML = "less";
+	document.getElementById("dropdowninfo1").style.display = "inline";
+	} else if (document.getElementById("dropdown1").innerHTML == "less") {
+	document.getElementById("dropdown1").innerHTML = "more";
+	document.getElementById("dropdowninfo1").style.display = "none";
+	}
+}
+
+function MoreAndLess2() {
+	if (document.getElementById("dropdown2").innerHTML == "more") {
+	document.getElementById("dropdown2").innerHTML = "less";
+	document.getElementById("dropdowninfo2").style.display = "inline";
+	} else if (document.getElementById("dropdown2").innerHTML == "less") {
+	document.getElementById("dropdown2").innerHTML = "more";
+	document.getElementById("dropdowninfo2").style.display = "none";
+	}
+}
+
+function MoreAndLess3() {
+	if (document.getElementById("dropdown3").innerHTML == "more") {
+	document.getElementById("dropdown3").innerHTML = "less";
+	document.getElementById("dropdowninfo3").style.display = "inline";
+	} else if (document.getElementById("dropdown3").innerHTML == "less") {
+	document.getElementById("dropdown3").innerHTML = "more";
+	document.getElementById("dropdowninfo3").style.display = "none";
+	}
+}
